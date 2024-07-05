23423dfcfa485c1ea5bc05bd5854eba282733098
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-07-05 14:56:26 +0100

    Improve launch message exceptions
    There are three subclasses of LtiException, giving a bit more
    information about what went wrong with a launch message.
    
    The error message strings are a bit more descriptive, as well.

--- pylti1p3/exception.py
+++ pylti1p3/exception.py
@@ -14,3 +14,24 @@ class LtiServiceException(LtiException):
         msg = f"HTTP response [{response.url}]: {str(response.status_code)} - {response.text}"
         super().__init__(msg)
         self.response = response
+
+
+class LtiJWTException(LtiException):
+    """
+        An error stemming from the JWT body of the message launch.
+    """
+
+
+class LtiMessageValidationException(LtiException):
+    """
+        An error encountered while validating an LTI launch message.
+    """
+    pass
+
+
+class LtiKeyException(LtiException):
+    """
+        An error to do with keys for the OAuth process.
+    """
+    pass
+
